01a619052204911e0c252d3c16174a3fdbd55a8f
zukanovicmilica <zukanovic.milica@gmail.com>
2018-03-10 00:20:35 +0100

    Implementirana klasa Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -6,17 +6,33 @@ import java.util.LinkedList;
 import menjacnicaInterfejs.InterfejsMenjacnice;
 
 public class Menjacnica implements InterfejsMenjacnice {
+	LinkedList<Valute> kursnaLista = new LinkedList<Valute>();
 
 	@Override
 	public void dodatiKurs(String nazivValute, Kurs noviKurs) {
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if (kursnaLista.get(i).getNazivValute().contains(nazivValute)) {
+				kursnaLista.get(i).kursevi.add(noviKurs);
+			}
+		}
 	}
 
 	@Override
 	public void obrisiKurs(String nazivValute, Kurs kursOdredjenogDana) {
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if (kursnaLista.get(i).getNazivValute().contains(nazivValute))
+				kursnaLista.get(i).kursevi.remove(kursOdredjenogDana);
+		}
 	}
 
 	@Override
 	public Kurs vratiKursNaOdredjeniDan(String nazivValute, GregorianCalendar datum) {
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if (kursnaLista.get(i).getNazivValute().contains(nazivValute))
+				for (int j = 0; j < kursnaLista.get(i).kursevi.size(); j++)
+					if (kursnaLista.get(i).kursevi.get(j).getDatum().compareTo(datum) == 0)
+						return kursnaLista.get(i).kursevi.get(j);
+		}
 		return null;
 	}
 
